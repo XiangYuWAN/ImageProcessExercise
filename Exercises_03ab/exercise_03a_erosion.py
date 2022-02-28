f09fe90c927798ddb6a9c5456b61eb5756a110a6
XiangYuWAN <wanwf8@163.com>
2022-02-28 02:28:16 +0100

    This is the image process exercise

--- Exercises_03ab/exercise_03a_erosion.py
+++ Exercises_03ab/exercise_03a_erosion.py
@@ -3,9 +3,9 @@ import numpy as np
 import img_nomcv_functions.functions as inff
 
 img1 = cv2.imread("immed_gray_inv.pgm", cv2.IMREAD_GRAYSCALE)
-img2 = cv2.imread("immed_gray_inv_20051123_ero2.pgm", cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread("immed_gray_inv_20051123_ero1.pgm", cv2.IMREAD_GRAYSCALE)
 #this is a function of erode
-i = 2
+i = 1
 kernel = np.ones(shape=(2*i+1, 2*i+1))
 def erode_image(image, kernel, ki):
     kernel_size = kernel.shape[0]
@@ -15,21 +15,20 @@ def erode_image(image, kernel, ki):
     if (image.max() != 255) or (image.min() != 0):
         raise ValueError("input image's pixel value must be grade")
 
-    e_image = np.zeros(shape=(image.shape[0],image.shape[1]))
-    center_move = int(ki)
+    e_image = np.zeros((image.shape[0],image.shape[1]), np.uint8)
 
-    for i in range(center_move, image.shape[0]-kernel_size+1):
-        for j in range(center_move, image.shape[1]-kernel_size+1):
-            e_image[i, j] = np.min(image[i-center_move:i+center_move, j-center_move:j+center_move])
+    for i in range(ki, image.shape[0]-kernel_size+1):
+        for j in range(ki, image.shape[1]-kernel_size+1):
+            e_image[i-ki : i+ki+1, j-ki : j+ki+1] = np.min(image[i-ki : i+ki+1, j-ki : j+ki+1])
+            # ki is the size of kernel(2 * ki + 1)
     return e_image
 
 ################
-erosion2 = cv2.erode(img1,kernel,iterations=1)
 e_image = erode_image(img1, kernel, i)
 result = inff.img_compare(img2, e_image)
 print(result)
 
-cv2.imshow("erode", erosion2)
+cv2.imshow("erode", e_image)
 cv2.waitKey(0)
 ##################
 '''
